f5e6b27c50f2b095a0d83807b7322817d8db1be6
KevinZav <kevinzavala.42@hotmail.com>
2023-06-07 12:29:32 -0600

    feat: add testing page

---
+++ src/app/pages/testing/TestingPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-green-light py-xs px-lg mx-lg w-[200px] text-start">
+    <typography variant="h2" text="Testing page" extra-styles="text-blue-500 bold"></typography>
+    <indicator :variant="'yellow-warning'" :text="'hola'"></indicator>
+  </div>
+</template>
+<script setup lang="ts">
+import Typography from '@components/Typography/Typography.vue';
+import Indicator from '@components/Indicator/Indicator.vue';
+</script>
