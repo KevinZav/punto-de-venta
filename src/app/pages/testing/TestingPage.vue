8004f9a068e8c1632ab0161ed7ddf4d390e2659b
KevinZav <kevinzavala.42@hotmail.com>
2023-06-10 12:48:11 -0600

    chore: add badge example in testing page

--- src/app/pages/testing/TestingPage.vue
+++ src/app/pages/testing/TestingPage.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="bg-green-light py-xs px-lg mx-lg w-[200px] text-start">
+  <div class="text-start">
     <typography variant="h2" text="Testing page" extra-styles="text-blue-500 bold"></typography>
     <indicator :variant="'yellow-warning'" :text="'hola'"></indicator>
+    <badge variant="badge-black" text="Vinos" @on-dismiss="dismissBadge" :dismissable="true"></badge>
   </div>
 </template>
 <script setup lang="ts">
 import Typography from '@components/Typography/Typography.vue';
 import Indicator from '@components/Indicator/Indicator.vue';
+import Badge from '@components/Badge/Badge.vue';
+
+const dismissBadge = () => console.log('badge dismiss')
 </script>
